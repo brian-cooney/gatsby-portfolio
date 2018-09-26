2b1262238ac647eb9894794cce26985ed8508ece
brian-cooney <bcooney@gmail.com>
2018-09-26 11:32:36 +0100

    Google analytics in the head

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,20 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-108460263-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "briancooney.co.uk",
+        head: true,       
       },
     },
     'gatsby-plugin-offline',
