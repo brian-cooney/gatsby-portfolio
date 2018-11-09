1f2badd86f8ad4ddb5a1329e6cd54a678916c2f2
brian-cooney <bcooney@gmail.com>
2018-11-09 00:01:08 +0000

    Add about page copy + Rebass grid

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,19 +14,21 @@ const DogImage = styled.img`
 const About = () => (
   <Layout>
     <Flex alignItems="center">
-      <Box mx="auto" p={5} fontSize={4} width={[1, 1, 1 / 2]}>
-        Digital fabrication tools and the maker movement were my gateway drugs
+      <Box mx="auto" p={2} fontSize={3} pt={4} width={[1, 1, 1 / 2]}>
+        "Digital fabrication tools and the maker movement were my gateway drugs
         into web development. Coming from a background in furniture and product
         design, I've swapped my chisel for text editor! <br /> To Kickstart my
         new career in software craftsmanship, in early 2017, I chose to study
-        with General Assembly on their flagship 3 Month{' '}
+        with General Assembly on their flagship 3 Month
         <a href="https://generalassemb.ly/education/web-development-immersive">
           Web Development Immersive course.
         </a>
-        &nbsp;Since that incredible experience I have worked for an agency and
-        SAAS product company building on my skill as developer. <br />
+        &nbsp;Since that incredible experience I have worked for a top&nbsp;
+        <a href="http://www.wcrs.com/">agency</a> and SAAS product&nbsp;
+        <a href="http://www.brandworkz.com/">company</a>
+        &nbsp; building on my skill as developer. <br />
         Right now, I'm focused on learning JavaScript deeply alongside React
-        GraphQl & node.
+        GraphQl & node".
       </Box>
     </Flex>
   </Layout>
