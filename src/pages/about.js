81a5db641f018dfc85778bb8222de07c6cc19105
brian-cooney <bcooney@gmail.com>
2018-11-09 00:53:15 +0000

    Add spotify playlist to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,10 +15,11 @@ const About = () => (
   <Layout>
     <Flex flexWrap="wrap">
       <Box mx="auto" pt={4} width={[1, 1 / 2, 1 / 2]}>
-        <Text p={2}>
+        <h3>About</h3>
+        <Text p={2} pl={0} pr={3}>
           "Digital fabrication tools and the maker movement were my gateway
           drugs into web development. Coming from a background in furniture and
-          product design, I've swapped my chisel for text editor! <br /> To
+          product design, I've swapped my chisel for a text editor! <br /> To
           Kickstart my new career in software craftsmanship, in early 2017, I
           chose to study with General Assembly on their flagship 3 Month
           <a href="https://generalassemb.ly/education/web-development-immersive">
