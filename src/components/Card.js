2f67e66848fc8d4cf2b171a00e7e6c16db1e0fa3
brian-cooney <bcooney@gmail.com>
2019-02-28 12:37:38 +0000

    Testing github webhooks

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,23 +5,19 @@ import styled from 'styled-components'
 const Card = props => (
   <div className="col-auto">
     <div className="portfolio-box mb-32">
-      <a
-        href={props.Link1}
-        className="portfolio-link-project1"
-      >
+      <a href={props.Link1} className="portfolio-link-project1">
         <img src={props.image} alt="project1" />
       </a>
-      <h6> <a
-        href={props.Link1}
-        target=""
-        rel="noopener"
-        > {props.heading}</a>
-        </h6>
+      <h6>
+        {' '}
+        <a href={props.Link1} target="" rel="noopener">
+          {' '}
+          {props.heading}
+        </a>
+      </h6>
       <p className="portfolio-project-desc">{props.content}</p>
       <div>
-        <a className="portfolio-project-asset">
-          {props.Technology}
-        </a>
+        <a className="portfolio-project-asset">{props.Technology}</a>
       </div>
     </div>
   </div>
